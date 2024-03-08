1d51b7707f17c362b155fe7aca47a4df5329422e
InduSecNiklas <99643556+InduSecNiklas@users.noreply.github.com>
2024-03-09 00:04:56 +0100

    build the page around iocs and new ioc database

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ connection_string = os.getenv('COSMOS_CONNECTION_STRING')
 
 # MongoDB client initialization
 client = MongoClient(connection_string)
-db = client['pokemon2']  # Database name
-pokemon_collection = db['collection2']  # Collection name
+db = client['iocs']  # Database name
+ioc_collection = db['ioccollection']  # Collection name
 
 # FastAPI app initialization
 app = FastAPI()
@@ -38,54 +38,26 @@ app.add_middleware(
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Model definition
-class Pokemon(BaseModel):
-    name: str
-    type: str
-    description: str
-
-# Adjust your Pydantic model for form data
-class PokemonForm(BaseModel):
-    name: str
-    type: str
-    description: str
-
-# Route Handlers
 @app.get("/")
 async def read_root(request: Request):
-    # Fetch all Pokémon names (or you can limit the number of documents to fetch if the collection is large)
-    all_pokemon = list(pokemon_collection.find({}, {'name': 1, '_id': 0}))
-    # Extract names and choose up to 3 random ones
-    all_names = [p['name'] for p in all_pokemon]
-    random_names = sample(all_names, min(len(all_names), 3))
-    # Get the full details of these random Pokémon
-    random_pokemon = [pokemon_collection.find_one({"name": name}) for name in random_names]
-    # Pass them to the template
+    # Fetch random IOCs (or you can limit the number of documents to fetch if the collection is large)
+    iocs_cursor = ioc_collection.find({}).limit(3)  # Adjust the limit as needed
+    iocs = list(iocs_cursor)
     return templates.TemplateResponse("index.html", {
         "request": request,
-        "random_pokemon": random_pokemon,
-        "weekday": datetime.now().strftime("%A")  # Add this line to include the weekday in the context
+        "random_iocs": iocs
     })
+
+@app.get("/ioc/")
+async def get_ioc(ioc: str = Query(None)):
+    if ioc:
+        ioc_data = ioc_collection.find_one({"ioc": ioc})
+        if ioc_data:
+            ioc_data.pop('_id', None)
+            return ioc_data
+    raise HTTPException(status_code=404, detail="IOC not found")
 
 @app.get("/weekday")
 async def get_weekday():
     weekday = datetime.now().strftime("%A")
     return {"weekday": weekday}
-
-#@app.post("/pokemon")
-#async def add_pokemon(name: str = Form(...), type: str = Form(...), description: str = Form(...)):
-#    pokemon_dict = {"name": name, "type": type, "description": description}
-#    existing_pokemon = pokemon_collection.find_one({"name": name})
-#    if existing_pokemon:
-    #        raise HTTPException(status_code=400, detail="Pokemon already exists")
-    #    pokemon_collection.insert_one(pokemon_dict)
-    #    return {"message": f"{name} added to the database!"}
-
-@app.get("/pokemon/")
-async def get_pokemon(name: str = Query(None)):
-    if name:
-        pokemon = pokemon_collection.find_one({"name": name})
-        if pokemon:
-            pokemon.pop('_id', None)
-            return pokemon
-    raise HTTPException(status_code=404, detail="Pokemon not found")
