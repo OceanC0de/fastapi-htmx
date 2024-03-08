4c8c3806f5560cc6e0c52f099570e975d6fb630b
InduSecNiklas <99643556+InduSecNiklas@users.noreply.github.com>
2024-03-08 22:59:01 +0100

    added favicon.ico and static folder

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ app.add_middleware(
 )
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Model definition
 class Pokemon(BaseModel):
